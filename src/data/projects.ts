f51d6757d4ffe8d0eb5933f54b67c9479f29d077
mrlangloi <rcheung1997@gmail.com>
2026-01-16 12:53:55 -0800

    reordered data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,25 +7,26 @@ export interface Project {
 }
 
 export const projects = [
-  {
-    id: 1,
-    title: "Stock Image Searcher",
-    description: "An image searcher using Pexels Image API to find and display stock images based on user queries",
-    tech: ["JavaScript", "React", "HTML/CSS"],
-    link: "https://github.com/mrlangloi/img-searcher"
-  },
-  {
-    id: 2,
-    title: "OBS Canvas Overlay",
-    description: "An Open Broadcaster Software (OBS) overlay for live-stream moderators to display media to viewers",
-    tech: ["JavaScript", "React", "Node.js", "Express", "MongoDB"],
-    link: "https://github.com/mrlangloi/canvas-overlay-client"
-  },
-  {
-    id: 3,
-    title: "Personal Calendar Tracker",
-    description: "A RESTful personal calendar and tracker application developed using Java and Spring Boot",
-    tech: ["Java", "Spring Boot", "Swing"],
-    link: "https://github.com/mrlangloi/ImportantDaysTracker"
-  },
+    {
+        id: 1,
+        title: "Personal Calendar Tracker",
+        description: "A RESTful personal calendar and tracker application developed using Java and Spring Boot",
+        tech: ["Java", "Spring Boot", "Swing"],
+        link: "https://github.com/mrlangloi/ImportantDaysTracker"
+
+    },
+    {
+        id: 2,
+        title: "OBS Canvas Overlay",
+        description: "An Open Broadcaster Software (OBS) overlay for live-stream moderators to display media to viewers",
+        tech: ["JavaScript", "React", "Node.js", "Express", "MongoDB"],
+        link: "https://github.com/mrlangloi/canvas-overlay-client"
+    },
+    {
+        id: 3,
+        title: "Stock Image Searcher",
+        description: "An image searcher using Pexels Image API to find and display stock images based on user queries",
+        tech: ["JavaScript", "React", "HTML/CSS"],
+        link: "https://github.com/mrlangloi/img-searcher"
+    },
 ];
