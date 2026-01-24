f98309f117a25c7e4abb4613b37209b3576878c7
mrlangloi <rcheung1997@gmail.com>
2026-01-23 20:05:13 -0800

    updated class names for CSS modules

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,34 +3,34 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faSteam } from '@fortawesome/free-brands-svg-icons/faSteam';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 
     return (
-        <nav className="nav-content">
-            <div className="nav-brand">
+        <nav className={styles["nav-content"]}>
+            <div className={styles["nav-brand"]}>
                 <a href="/">
                     <p>ricky c.</p>
                 </a>
             </div>
 
-            <ul className="nav-links">
+            <ul className={styles["nav-links"]}>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#about">About</a></li>
                 {/* <li><a href="#contact">Contact</a></li> */}
                 <li>
-                    <div className="nav-contacts">
-                        <a className="nav-contacts-icons" href="https://www.linkedin.com/in/ricky-c0997/" target="_blank" rel="noopener noreferrer">
+                    <div className={styles["nav-contacts"]}>
+                        <a className={styles["nav-contacts-icons"]} href="https://www.linkedin.com/in/ricky-c0997/" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faLinkedin} fontSize="24px" />
                         </a>
-                        <a className="nav-contacts-icons" href="https://www.github.com/mrlangloi" target="_blank" rel="noopener noreferrer">
+                        <a className={styles["nav-contacts-icons"]} href="https://www.github.com/mrlangloi" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faGithub} fontSize="24px" />
                         </a>
-                        <a className="nav-contacts-icons" href="https://www.steamcommunity.com/id/rikkiimaru/" target="_blank" rel="noopener noreferrer">
+                        <a className={styles["nav-contacts-icons"]} href="https://www.steamcommunity.com/id/rikkiimaru/" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faSteam} fontSize="24px" />
                         </a>
-                        <a className="nav-contacts-icons" href="mailto:rca162@sfu.ca" target="_blank" rel="noopener noreferrer">
+                        <a className={styles["nav-contacts-icons"]} href="mailto:rca162@sfu.ca" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faEnvelope} fontSize="24px" />
                         </a>
                     </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
 
 interface ProjectProps {
     title: string;
@@ -11,16 +13,16 @@ const ProjectCard = (props: ProjectProps) => {
     const { title, description, tech, link } = props;
 
     return (
-        <div className="card">
-            <div className="card-image">
+        <div className={styles.card}>
+            <div className={styles["card-image"]}>
                 <img src="https://placehold.co/600x400/png"></img>
             </div>
-            <div className="card-content">
+            <div className={styles["card-content"]}>
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <div className="tech-stack tech-stack-wrapper">
+                <div className={styles["tech-stack"]}>
                     {tech.map((t, index) => (
-                        <span key={index} className="tag">{t}</span>
+                        <span key={index} className={styles.tag}>{t}</span>
                     ))}
                 </div>
                 <a href={link} target="_blank" rel="noopener noreferrer">Github</a>
