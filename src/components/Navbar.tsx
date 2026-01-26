d0c757e2a4c299730090b04f75fa1cca68b14b09
mrlangloi <rcheung1997@gmail.com>
2026-01-25 22:46:56 -0800

    updated nav links href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,14 @@ const Navbar = () => {
     return (
         <nav className={styles["nav-content"]}>
             <div className={styles["nav-brand"]}>
-                <a href="/">
+                <a href="/portfolio">
                     <p>ricky c.</p>
                 </a>
             </div>
 
             <ul className={styles["nav-links"]}>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/about">About</a></li>
                 {/* <li><a href="#contact">Contact</a></li> */}
                 <li>
                     <div className={styles["nav-contacts"]}>
