abea21ecdf20d9f70eafb570beda5eadc42e66b1
Rcheu <rcheung1997@gmail.com>
2026-01-27 22:28:34 -0800

    added patterns

---
+++ src/data/patterns.ts
@@ -0,0 +1,21 @@
+import circuitBoard from '../assets/patterns/circuit-board.svg';
+import jigsaw from '../assets/patterns/jigsaw.svg';
+import iLikeFood from '../assets/patterns/i-like-food.svg';
+import bubbles from '../assets/patterns/bubbles.svg';
+import floatingCogs from '../assets/patterns/floating-cogs.svg';
+
+export const PATTERNS = [
+    circuitBoard,
+    jigsaw,
+    iLikeFood,
+    bubbles,
+    floatingCogs,
+];
+
+export const COLORS = [
+    '#f87171',
+    '#60a5fa',
+    '#34d399',
+    '#fbbf24',
+    '#818cf8'
+];
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,12 @@
+/// <reference types="vite/client" />
+
 // this file allows TypeScript to understand CSS module imports
+declare module '*.svg' {
+  const content: string;
+  // export default content;
+}
+
 declare module '*.module.css' {
   const classes: { [key: string]: string };
-  export default classes;
+  // export default classes;
 }
